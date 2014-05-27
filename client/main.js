5c697b25b48139cb40e96c2f3852e5df5374694c
Luis P <luis.prieto@epfl.ch>
2014-05-27 18:53:59 +0200

    first implementation of the classroom state visualization, based on a (simplified) classroom state model

--- client/main.js
+++ client/main.js
@@ -1,21 +1,47 @@
 //Note: The classroom data should be available under the Classrooms variable! (in classrooms.js)
 
+var classroomData = undefined;
+var devicesHistoryCount = new Array();
+var STEPS_PER_MAP = 12;
+var game = undefined;
+var pausedSprite = undefined;
+var bugInitialState = {
+    completedMap: 0,
+    stepsDone: 0,
+    stepsToGo: STEPS_PER_MAP,
+};
+var bugStates = new Array();//this will store each bug/device state
+var bugGroups = new Array();//this will store the sprites for each bug/device line of the game
+
+
+
 //Reactive code to react to changes in the classroom data
 Deps.autorun(function() {
     console.log("entering autorun");
-  var classroomData = Classrooms.findOne({_id: "9AWkshbxHiE45Aci7"});
+  classroomData = Classrooms.findOne({_id: "9AWkshbxHiE45Aci7"});
 
   if(classroomData){
         var status;
 
         //we black out the classroom if so set
         if(classroomData.global.paused){
-            document.body.style.background = 'black';  
-            status = classroomData.name +  " has been stopped!\n";
+
+            if(Template.camera.rendered || Template.nocamera.rendered){
+                //document.body.style.background = 'black';  
+                status = classroomData.name +  " has been stopped!\n";
+            }
+
+            if(game) pauseGame();
+
         } 
         else{
-            document.body.style.background = 'white';  
-            status = classroomData.name +  " is transcurring normally.\n";
+            if(Template.camera.rendered || Template.nocamera.rendered){
+                //document.body.style.background = 'white';  
+                status = classroomData.name +  " is transcurring normally.\n";
+            }
+
+            if(game) unpauseGame();
+
         } 
 
         if(classroomData.devices.length>0){
@@ -28,6 +54,9 @@ Deps.autorun(function() {
                         status += "Tags present in device "+classroomData.devices[i]+": "+device.current.presentTags.toString()+"\n";
                     }
                 }
+
+                //TODO: for now, we just count the number of entries in each device's history
+                devicesHistoryCount[i] = DeviceHistory.find({deviceid: classroomData.devices[i]}).count();
 
             }
 
@@ -52,11 +81,27 @@ Deps.autorun(function() {
 });
 
 
+function pauseGame(){
+    if(!game.paused){
+        pausedSprite = game.add.sprite(game.world.centerX, game.world.centerY, 'pause');
+        pausedSprite.anchor.setTo(0.5, 0.5);
+        game.paused = true;
+    }
+}
+
+function unpauseGame(){
+    if(game.paused && pausedSprite){
+        game.paused = false;
+        pausedSprite.destroy();
+        pausedSprite = undefined;
+    }
+}
+
+
 // chilitags variables
 var fpsText, start;
 var tagBlackout = "tag_1";
 var tagAnother = "tag_2";
-
 var rendered = false;
 
 // If it is the camera version, we initialize the whole camera and chilitags setup
@@ -82,6 +127,123 @@ Template.nocamera.rendered = function() {
         initSimpleClassroom();
     }
 }
+
+
+Template.visualization.rendered = function() {
+
+    console.log("entering visualization rendered");
+
+    if (!rendered) {
+        //prevent to do the initialization twice
+        rendered = true;
+        game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+
+    }
+
+
+}
+
+
+
+
+function preload () {
+    
+    game.stage.backgroundColor = '#FFFFFF';
+    game.load.image('bug', 'ladybug_sq64.png');
+    game.load.image('pause', 'pause.png');
+    game.load.image('step','ballgreen32.png');
+    game.load.image('togo','ballwhite32.png');
+    game.load.image('map','map64.png');
+
+}
+
+//Some constants for the graphical representation, in pixels
+var INITIAL_X = 50;
+var INITIAL_Y = 50;
+var Y_STEP = 150;
+var MAP_STEP = 75;
+var BALL_STEP = 32;
+var BUG_STEP = 64;
+
+function create () {
+
+    //var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'bug');
+    //We create the groups for the team bugs
+    if(classroomData){
+        if(classroomData.devices.length>0){
+            bugGroups = new Array(classroomData.devices.length);
+            bugStates = new Array(classroomData.devices.length);
+        }
+        for(var i = 0; i<classroomData.devices.length; i++){
+            bugGroups[i] = game.add.group();
+            bugGroups[i].deviceid = classroomData.devices[i];//we add the device id, just in case we want to check the order is being maintained
+            //console.log('within create(), initial state is '+bugInitialState+' and there are states: '+bugStates.length);
+            bugStates[i] = JSON.parse(JSON.stringify(bugInitialState));//we clone the initial state data and add it to the array of states
+            bugStates[i].deviceid = classroomData.devices[i];//we add the device id, just in case we want to check the order is being maintained
+            var yval = INITIAL_Y + (i * Y_STEP);
+            //console.log('calling drawing for '+bugGroups[i]+' and '+bugStates[i]);
+            drawRowState(bugGroups[i], bugStates[i], yval);            
+        }
+    }//TODO: what if we have not received the classroom data yet??
+
+}
+
+
+function drawRowState(group, state, yval){
+
+    group.removeAll(true); //We cleanup all sprites in this group
+    var currentx = INITIAL_X; //to keep track of the drawing of everything in a row
+    for(var i=0;i<state.completedMap;i++){
+        var newSprite = group.create(currentx,yval,'map');
+        newSprite.anchor.setTo(0.5, 0.5);
+        currentx = currentx + MAP_STEP;
+    }
+
+    for(var i=0;i<state.stepsDone;i++){
+        var newSprite = group.create(currentx,yval,'step');
+        newSprite.anchor.setTo(0.5, 0.5);
+        currentx = currentx + BALL_STEP;
+    }
+
+    var bugSprite = group.create(currentx,yval,'bug');
+    bugSprite.anchor.setTo(0.5, 0.5);
+    currentx = currentx + BUG_STEP;
+
+    for(var i=0;i<state.stepsToGo;i++){
+        var newSprite = group.create(currentx,yval,'togo');
+        newSprite.anchor.setTo(0.5, 0.5);
+        currentx = currentx + BALL_STEP;
+    }    
+
+}
+
+
+
+function update(){
+    if(classroomData){
+        for(var i = 0; i<classroomData.devices.length; i++){
+            bugStates[i] = calculateCurrentState(i);
+            var yval = INITIAL_Y + (i * Y_STEP);
+            drawRowState(bugGroups[i], bugStates[i], yval); //TODO: probably, it would be more efficient to move sprites around instead of recreating them each time!   
+        }
+    }    //TODO: what if we have not received the classroom data yet??    
+}
+
+function calculateCurrentState(device){
+
+    var count = devicesHistoryCount[device];
+    var completedMaps = Math.floor(count/STEPS_PER_MAP);
+    var steps = count - (STEPS_PER_MAP * completedMaps);
+    var togo = STEPS_PER_MAP-steps;
+    var currentstate = {
+        completedMap: completedMaps,
+        stepsDone: steps,
+        stepsToGo: togo,
+    };
+    //console.log('current state: '+completedMaps+' completed, '+steps+' steps, '+togo+' to go.')
+    return currentstate;
+}
+
 
 
 function initSimpleClassroom() {
@@ -120,7 +282,7 @@ function initPaprikaEvents() {
 }
 
 function blackoutScreen() {
-    document.body.style.background = 'black';
+    //document.body.style.background = 'black';
 
     //Modify the database if the classroom was not already blacked
     var classroomData = Classrooms.findOne({_id: "9AWkshbxHiE45Aci7"});
@@ -132,7 +294,7 @@ function blackoutScreen() {
         var deviceData = Devices.findOne({_id: "LziCQ4oJQ7bpQv7sA"});
         var devId = deviceData._id;
         if(deviceData.current.presentTags){
-            console.log("updating device info for device "+deviceData.id);
+            //console.log("updating device info for device "+deviceData.id);
             Devices.update({_id: devId},{$push: {"current.presentTags": tagBlackout}});
         }
 
@@ -143,7 +305,7 @@ function blackoutScreen() {
 }
 
 function normalScreen(){
-    document.body.style.background = 'white';    
+    //document.body.style.background = 'white';    
 
     //Modify the database if the classroom was not already unblacked
     var classroomData = Classrooms.findOne({_id: "9AWkshbxHiE45Aci7"});
