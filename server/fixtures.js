96421e611a6f394303c59276cf6df137b4748f07
Luis P <luis.prieto@epfl.ch>
2014-07-28 16:39:32 +0200

    Added a second lamp to the initial device database, for testing

--- server/fixtures.js
+++ server/fixtures.js
@@ -8,14 +8,14 @@ if (Classrooms.find().count() === 0) {
 			pauserDevice: '',
 			masterHint: ''
 		},
-		devices: ["LziCQ4oJQ7bpQv7sA", "mmLHNnR5iWk87Xx2i"]
+		devices: ["LziCQ4oJQ7bpQv7sA", "mmLHNnR5iWk87Xx2i", "uAXTQLmEnD5GNDpqy"]
 	});
 }
 
 if (Devices.find().count() === 0) {
 	Devices.insert({
 		_id: "LziCQ4oJQ7bpQv7sA",
-		name: 'Red Lamp',
+		name: 'LP Desktop',
 		current:{
 			activity: {
 				id: 1,
@@ -34,7 +34,26 @@ if (Devices.find().count() === 0) {
 	});
 	Devices.insert({
 		_id: "mmLHNnR5iWk87Xx2i",
-		name: 'Blue Lamp',
+		name: 'Yellow Lamp - transformer05',
+		current:{
+			activity: {
+				id: 1,
+				name: 'Kill the bug',
+				state: {
+					completedMaps: 0,
+					stepsDone: 0,
+					stepsToGo: 0,
+					hintPresent: '',
+					wrongMoves: 0
+				}
+			},
+			presentTags: []
+		}
+
+	});
+	Devices.insert({
+		_id: "uAXTQLmEnD5GNDpqy",
+		name: 'Red Lamp - transformer00',
 		current:{
 			activity: {
 				id: 1,
